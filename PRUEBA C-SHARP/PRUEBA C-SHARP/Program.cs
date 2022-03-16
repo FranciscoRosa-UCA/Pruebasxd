c7a89e53cbe0c84368edf61484d9e6baa8cea20f
FranciscoRosa-UCA <00046821@uca.edu.sv>
2022-03-15 22:20:35 -0600

    COMMIT PC

---
+++ PRUEBA C-SHARP/PRUEBA C-SHARP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PRUEBA_C_SHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("a");
+
+            Thread.Sleep(2000);
+        }
+    }
+}

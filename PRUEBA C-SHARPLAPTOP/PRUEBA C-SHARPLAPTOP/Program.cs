9383e02c916bef8d6c2d82876f52436672b615a1
FranciscoRosa-UCA <00046821@uca.edu.sv>
2022-03-15 21:12:36 -0600

    commit 1

---
+++ PRUEBA C-SHARPLAPTOP/PRUEBA C-SHARPLAPTOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRUEBA_C_SHARPLAPTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LAPTOP");
+        }
+    }
+}
